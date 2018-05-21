4a3eec87bb4f5d0f5a590bc67df640164455f6ab
hn <heathernolis@gmail.com>
2018-05-20 19:50:10 -0700

    added a fitness function

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import random
 
+# given a board, this function returns the number of good queens
+def goodQueens(board):
+    score = 0
 
+    #for every item in the board
+    for i in range(len(board)):
+            # check the next item
+            for j in range(i+1, len(board)):
+                #if the rows are not the same
+                if i[0] != j[0]:
+                    #if the columns are not the same
+                    if i[1] != j[1]:
+                        #if they are not on a diagonal
+                        if abs(i[1] - j[1]) != abs(i[0] - j[0]):
+                            score += 1
+
+    return score
 
 # set size of board
 boardSize = 4
